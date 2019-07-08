a1d2dd796636597959da646d7b97116bfb037694
Laura Jimenez <laurajimenez@laura-macbook-pro.clearc2.com>
2019-07-08 11:15:39 -0500

    blog page edit

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import BlogList from '../templates/blog-list-template'
+
+class Blog extends React.Component {
+  render() {
+    return (
+      <BlogList />
+    )
+  }
+}
+
+export default Blog
