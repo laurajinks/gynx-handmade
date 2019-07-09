ff8b4627addd5ee141ffc63b40967ae0609c8f62
Laura Jimenez <laurajimenez@laura-macbook-pro.clearc2.com>
2019-07-09 08:29:37 -0500

    404 mssg

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,7 +13,7 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
         <h1>Page Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>This page doesn't exist... yet</p>
         <Bio />
       </Layout>
     )
